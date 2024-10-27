40a1977ea2f0c4105f99ceaf711ace600aa473bb
Alonesa <alenakrasnov@mail.ru>
2024-10-27 16:39:14 +0000

    divided for a big components

---
+++ src/view/createEventView.js
@@ -0,0 +1,68 @@
+import {createElement} from '../render';
+
+function createEventView() {
+  return `
+    <div class="trip-main">
+          <section class="trip-main__trip-info  trip-info">
+            <div class="trip-info__main">
+              <h1 class="trip-info__title">Amsterdam &mdash; Chamonix &mdash; Geneva</h1>
+
+              <p class="trip-info__dates">18&nbsp;&mdash;&nbsp;20 Mar</p>
+            </div>
+
+            <p class="trip-info__cost">
+              Total: &euro;&nbsp;<span class="trip-info__cost-value">1230</span>
+            </p>
+          </section>
+
+          <div class="trip-main__trip-controls  trip-controls">
+            <div class="trip-controls__filters">
+              <h2 class="visually-hidden">Filter events</h2>
+              <form class="trip-filters" action="#" method="get">
+                <div class="trip-filters__filter">
+                  <input id="filter-everything" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="everything" checked>
+                  <label class="trip-filters__filter-label" for="filter-everything">Everything</label>
+                </div>
+
+                <div class="trip-filters__filter">
+                  <input id="filter-future" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="future">
+                  <label class="trip-filters__filter-label" for="filter-future">Future</label>
+                </div>
+
+                <div class="trip-filters__filter">
+                  <input id="filter-present" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="present">
+                  <label class="trip-filters__filter-label" for="filter-present">Present</label>
+                </div>
+
+                <div class="trip-filters__filter">
+                  <input id="filter-past" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="past">
+                  <label class="trip-filters__filter-label" for="filter-past">Past</label>
+                </div>
+
+                <button class="visually-hidden" type="submit">Accept filter</button>
+              </form>
+            </div>
+          </div>
+
+          <button class="trip-main__event-add-btn  btn  btn--big  btn--yellow" type="button" disabled>New event</button>
+        </div>
+  `;
+}
+
+export default class CreateEvent {
+  getTemplate() {
+    return createEventView();
+  }
+
+  getElement() {
+    if (!this.element) {
+      this.element = createElement(this.getTemplate());
+    }
+    return this.element;
+  }
+
+  removeElement() {
+    this.element = null;
+  }
+}
+
